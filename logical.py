153cd8e30db532b2b7f91339a323c07fb0de7032
unknown <02230059.cst@rub.edu.bt>
2024-04-22 17:43:41 -0400

    logical

---
+++ logical.py
@@ -0,0 +1,18 @@
+#example 1
+x = 5
+print(x > 5 and x < 10)
+#example 2
+y = 12
+print(y > 10 and y % 5 == 0)
+#xample 1
+x = 5
+print(not (y > 3 and x < 10))
+y = 12
+print(not (y > 10 and y % 5 == 0))
+##practice problem
+x = int(input("Enter your age :"))
+input("Are you student (yes or no) :")
+if x==12 and 13<=x>=18:
+    print("you are eligible for discount")
+elif x>18:
+    print("you are not eligible")    
