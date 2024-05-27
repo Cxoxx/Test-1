870318a57f04f91e41c129fe5b9c6903c066ed8b
unknown <02230059.cst@rub.edu.bt>
2024-05-27 20:44:11 +0600

    student grades

--- student_grade.py
+++ student_grade.py
@@ -1,13 +1,14 @@
-num_student = int(input("Enter the number of student: "))
-i = 1
-while i <= num_student:
-    total_grade=0
-    num_subject = int(input(f"Enter the number of subject for student {i}:")) 
+num_students = int(input("Enter the number of students: "))
 
-    for j in range(1,num_subject +1):
-        grade = float(input(f"Enter subject{j} grade for stuudent {i}:"))
-        total_grade += grade
+i = 1
+while i <= num_students:
+    total_grade = 0
+    Num_of_subject =int(input(f"Enter the number of subject for student {i}:"))
+
+    for j in range(1, Num_of_subject+1):
+        grades = float(input(f"Enter grade for subject {j} of student {i} :"))
+        total_grade += grades
     
-    average_grade = total_grade/ num_subject
-    print(f"Average grade for student {i}: {average_grade: .2f} ")
+    average_grade = total_grade / Num_of_subject
+    print(f"Average grade for student {i} is {average_grade}:")
     i += 1
